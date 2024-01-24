8275e83cedd6e02fc44edd6b396d53ea66d3a560
LUIS MARTINEZ ALFARO <luismartinezalfaro@192.168.1.83>
2024-01-24 06:49:05 -0600

    ejercicios en python

---
+++ suma.py
@@ -0,0 +1,17 @@
+#Ejemplo suma
+
+print("suma de 3 numeros")
+
+a=int(input("Dame el primer valor: "));
+b=int(input("Dame el segundo valor: "));
+c=int(input("Dame el tercer valor: "));
+
+suma=a+b+c
+resta=a-b-c
+multiplicacion=a*b*c
+division=a/b/c
+
+print("la suma de",a," mas",b," mas",c," es",suma)
+print("la resta de",a," mas",b," mas",c," es",resta)
+print("la multiplicación de",a," mas",b," mas",c," es",multiplicacion)
+print("la división de",a," mas",b," mas",c," es",division)
